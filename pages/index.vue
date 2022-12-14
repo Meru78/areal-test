c53718e24b1a12b2f240fac2f96c33867900c970
mmelnikov <mmelnikov@er76.ru>
2022-12-14 19:42:37 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="8">
+      <v-card>
+        <v-card-text>
+          <v-textarea outlined label="Введите текст"></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn absolute right v-on:click="">
+            Получить ссылку
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
